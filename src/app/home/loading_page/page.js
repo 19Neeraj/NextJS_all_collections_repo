30887dd141bf090b251a5cacca720c7f8b126b5b
19Neeraj <19futuretech@gmail.com>
2024-04-30 19:29:41 +0530

    loading page setup

--- src/app/home/loading_page/page.js
+++ src/app/home/loading_page/page.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 async function takeTime(){
   await new Promise((resolve)=>{
-    setTimeout(resolve,2000);
+    setTimeout(resolve,3000);
   })
 }
 export default async function Loading_page() {
