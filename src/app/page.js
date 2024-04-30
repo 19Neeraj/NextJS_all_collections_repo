cba6ff2b6c0746fd31f605232392fbb01d3e6ca6
19Neeraj <19futuretech@gmail.com>
2024-04-30 21:44:49 +0530

    .

--- src/app/page.js
+++ src/app/page.js
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-   <main>
-    try
+   <main className="w-[100vw] h-[100vh] bg-slate-800 text-white flex justify-center items-center text-5xl font-bold">
+    Exploring NextJS Features and Functionality 
    </main>
   );
 }
