f6f40f3263e233295617233496a9c8eda578290a
19Neeraj <19futuretech@gmail.com>
2024-05-01 14:22:50 +0530

    fixed the error that coming while creating a user

---
+++ src/helper/database.js
@@ -0,0 +1,18 @@
+
+import mongoose from "mongoose";
+
+
+export const  connectdb = async() => {
+  
+    try{
+const {connection}=await mongoose.connect(process.env.MONGO_DB_URL,{dbName:"Next_APP"});
+console.log("we are connected with database now DB Name :- " , connection.name );
+// console.log(connection);
+
+
+    }
+    catch(error){
+console.log("failed to connect with database");
+    }
+}
+
---
+++ src/models/user.js
@@ -0,0 +1,35 @@
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, "Name is required"]
+    },
+    email: {
+        type: String,
+        required: [true, "Email is required"],
+        unique: [true,"Email id is existing "] ,// Ensures uniqueness of email
+        // lowercase: true, // Converts email to lowercase before saving
+        // trim: true, // Removes whitespace from the beginning and end of email
+        // validate: {
+        //     validator: function(value) {
+        //         // Regular expression for email validation
+        //         return /\S+@\S+\.\S+/.test(value);
+        //     },
+        //     message: props => `${props.value} is not a valid email`
+        // }
+    },
+    password: {
+        type: String,
+        required: [true, "Password is required"],
+        // minlength: [6, "Password must be at least 6 characters long"]
+    },
+    about: {
+        type: String,
+        trim: true // Removes whitespace from the beginning and end of about
+    }
+});
+
+// Export the model
+const User = mongoose.models.User || mongoose.model("User", UserSchema);
+export default User;
